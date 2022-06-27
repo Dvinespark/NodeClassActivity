04a2a8cd9a3ca7c1fffee1db234d0c1f445519a0
shubhas <nimtsubash@gmail.com>
2022-06-26 20:13:21 -0400

    minor changes

--- HelloWorld/routes/index.js
+++ HelloWorld/routes/index.js
@@ -1,30 +1,41 @@
 var express = require('express');
 var router = express.Router();
+const { MongoClient} = require('mongodb');
 
 
-
+let collection_count = 0;
 // Doing mongo thing here
 
-// let data = {};
-// // console.log("owoww");
-let collection_count = 0;
-const { MongoClient} = require('mongodb');
-const uri = "mongodb+srv://subash:subash123@cluster0.jdzzw.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useUnifiedTopology: true });
+let data = [];
 
 
-client.connect().then(client => {
-  console.log("wow");
-  collection_count = client.db("subash").listCollections().toArray();
-  console.log(collection_count);
-  console.log("I am here");
-})
-.then(cols => {console.log("collections" + cols)})
-.finally (client.close());
+function printData() {
+
+  // console.log("owoww");
+  const uri = "mongodb+srv://subash:subash123@cluster0.jdzzw.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+
+  client.connect().then(client => {
+    console.log("wow");
+    collection_count = client.db('Subash').listCollections().toArray((err, collections) => {
+      console.log(collections);
+      data.push(collections.name);
+    });
+    console.log("I am here");
+  })
+  .then(data1 => {
+    console.log('data: ' ,data);
+    console.log('data1:', data1);
+  })
+  .finally (client.close());
+}
 
 
 /* GEThome page. */
 router.get('/', function(req, res, next) {
+  console.log("Console message");
+  printData();
   res.render('index', { title: collection_count });
 });
 
